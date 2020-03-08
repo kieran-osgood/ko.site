2ec2a466c065d706fb6e5ef34541a8200a77e1bf
Kieran Osgood <kieran@analogrepublic.com>
2020-03-08 08:06:37 +0000

    Add all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,21 +5,15 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = ({ path }) => (
+const AboutPage = ({ path }) => (
   <Layout path={path}>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
+    <SEO title="About" />
+    <h1>About</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
-export default IndexPage
+export default AboutPage
