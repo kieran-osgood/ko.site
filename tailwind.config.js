03048dda4df3b1d8d007a91c528d8fe576eecca5
Kieran Osgood <kieran@analogrepublic.com>
2020-03-08 08:07:07 +0000

    Add gatsby css plugins and configs - set breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ module.exports = {
       padding: '2rem'
     },
     screens: {
-      'sm': '640px',
+      'sm': '375px',
       'md': '768px',
       'lg': '1024px',
-      'xl': '1280px',
+      'xl': '1440px',
      },
      inset: {
      	"1": "1.5rem"
