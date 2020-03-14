c394824239bb2fcd0b9f282bca86af0c093130e8
Kieran Osgood <kieran@analogrepublic.com>
2020-03-14 15:45:45 +0000

    update profilecard icons to size better

--- src/components/profilecard.js
+++ src/components/profilecard.js
@@ -20,19 +20,17 @@ export const ProfileCard = () => {
 				className='rounded-tl-md rounded-tr-md overflow-hidden shadow-lg'
 				style={{ backgroundColor: '#EDEDED', height: '280px' }}
 			>
-				<div>
-					<div className='w-64 mx-auto text-center text-black relative pt-16'>
+					<div className='w-4/5 mx-auto text-center text-black relative pt-16'>
 						<h3 className='text-2xl text-red-700'>Kieran Osgood</h3>
 						<p className='pt-2'>Web Developer</p>
 						<p className='pt-2'>London</p>
 
 						<LanguagesList />
 					</div>
-				</div>
 			</div>
 
 			<div
-				className=' rounded-bl-lg rounded-br-lg flex justify-center items-center shadow-lg text-red-400'
+				className='rounded-bl-lg rounded-br-lg flex justify-center items-center shadow-lg text-red-400'
 				style={{ backgroundColor: '#C4C4C4', height: '155px' }}
 			>
 				{/* <div style={{backgroundColor: '#C4C4C4'}}> */}
@@ -45,9 +43,9 @@ export const ProfileCard = () => {
 const LanguagesList = () => {
 	const stripPathing = string => string.match(/(?:.\/)+(\w*)+(?:\.svg)/)[1]
 	return (
-		<div className='flex flex-row justify-around pt-6'>
+		<div className='flex flex-row justify-around pt-6 h-16 justify-around'>
 			{svgs.map(svg => (
-				<img src={svg.file} alt={`${stripPathing(svg.path)} logo`} />
+				<img src={svg.file} className='h-full px-1' alt={`${stripPathing(svg.path)} logo`} />
 			))}
 		</div>
 	)
