47ad6cc94821d6baa07f9d337ebc14448ee7a3c2
Kieran Osgood <kieran@analogrepublic.com>
2020-03-14 15:59:08 +0000

    Add social icons to profilecard

--- src/components/profilecard.js
+++ src/components/profilecard.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Image from './image'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import Github from '../images/socials/Github.svg'
+import LinkedIn from '../images/socials/LinkedIn.svg'
+import Twitter from '../images/socials/Twitter.svg'
 
 // Pulls * required SVGs + maps to array of objects: {path: ..., file: ...}
 const reqSvgs = require.context('../images/languages', true, /\.svg$/)
@@ -33,6 +36,7 @@ export const ProfileCard = () => {
 				className='rounded-bl-lg rounded-br-lg flex justify-center items-center shadow-lg text-red-400'
 				style={{ backgroundColor: '#C4C4C4', height: '155px' }}
 			>
+				<SocialsList />
 				{/* <div style={{backgroundColor: '#C4C4C4'}}> */}
 				{/* <ContactStrip /> */}
 				{/* </div> */}
@@ -50,21 +54,31 @@ const LanguagesList = () => {
 		</div>
 	)
 }
-
+const SocialsList = () => {
+	return (
+		<div className='flex flex-row justify-around pt-6 h-32 justify-around w-4/5'>
+			{socials.map(social => (
+				<a href={social.url} target="_blank" >
+					<img src={social.icon} className='h-16 w-16 px-1' alt={`${social.platform} logo`} />
+				</a>
+			))}
+		</div>	
+	)
+}
 const socials = [
 	{
-		platform: 'github',
-		url: 'github.com/kierano547',
-		icon: '../images/github.svg',
+		platform: 'twitter',
+		url: 'https://github.com/kierano547',
+		icon: Github,
 	},
 	{
 		platform: 'linkedin',
-		url: 'linkedin.com/kieranosgood',
-		icon: '../images/linkedin.svg',
+		url: 'https://linkedin.com/kieranosgood',
+		icon: LinkedIn,
 	},
 	{
 		platform: 'twitter',
-		url: 'twitter.com/kieranbosgood',
-		icon: '../images/twitter.svg',
+		url: 'https://twitter.com/kieranbosgood',
+		icon: Twitter,
 	},
 ]
