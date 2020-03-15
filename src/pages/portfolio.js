9a1a693e22e42d94303bde82cf5d5b313488e397
Kieran Osgood <kieran@analogrepublic.com>
2020-03-15 06:21:09 +0000

    Begin including pure-react-carousel

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {
+	CarouselProvider,
+	Slider,
+	Slide,
+	ButtonBack,
+	ButtonNext,
+} from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
+import config from '../../tailwind.config'
+import useWindowSize from '../hooks/useWindowSize'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const Portfolio = ({ path }) => {
+	const { width } = useWindowSize()
 	return (
 		<Layout path={path}>
 			<SEO title='Page two' />
-			<h1>Portfolio</h1>
-			<p>Welcome to page 2</p>
-			<Link to='/'>Go back to the homepage</Link>
+
+			{width < config.theme.screens.md && (
+				<CarouselProvider
+					naturalSlideWidth={100}
+					naturalSlideHeight={125}
+					totalSlides={3}
+				>
+					<Slider>
+						<Slide index={0}>I am the first Slide.</Slide>
+						<Slide index={1}>I am the second Slide.</Slide>
+						<Slide index={2}>I am the third Slide.</Slide>
+					</Slider>
+				</CarouselProvider>
+			)}
 		</Layout>
 	)
 }
