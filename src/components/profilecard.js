52113798c1399cd38a06c168e595bef8e5ecffe9
Kieran Osgood <kieran@analogrepublic.com>
2020-03-15 06:01:18 +0000

    Remove inline styling

--- src/components/profilecard.js
+++ src/components/profilecard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from './image'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+
 import Github from '../images/socials/Github.svg'
 import LinkedIn from '../images/socials/LinkedIn.svg'
 import Twitter from '../images/socials/Twitter.svg'
@@ -12,24 +11,21 @@ const svgs = reqSvgs.keys().map(path => ({ path, file: reqSvgs(path) }))
 
 export const ProfileCard = () => {
 	return (
-		<div style={{ maxHeight: '525px', maxWidth: '429px' }}>
-			<div
-				className='w-32 h-32 mx-auto relative overflow-visible '
-				style={{ top: '64px', height: '128px', width: '128px' }}
-			>
-				<Image filename='profile.png' />
+		<div className='profile-card'>
+			<div className='w-32 h-32 pt-16 mx-auto relative overflow-visible'>
+				<Image />
 			</div>
 			<div
-				className='rounded-tl-md rounded-tr-md overflow-hidden shadow-lg'
+				className='rounded-tl-md rounded-tr-md overflow-hidden shadow-lg bg-gray-100'
 				style={{ backgroundColor: '#EDEDED', height: '280px' }}
 			>
-					<div className='w-4/5 mx-auto text-center text-black relative pt-16'>
-						<h3 className='text-2xl text-red-700'>Kieran Osgood</h3>
-						<p className='pt-2'>Web Developer</p>
-						<p className='pt-2'>London</p>
+				<div className='w-4/5 mx-auto text-center text-black relative pt-16'>
+					<h3 style={{ color: 'black'}} className='text-2xl'>Kieran Osgood</h3>
+					<p style={{ color: 'black'}} className='pt-2'>Web Developer</p>
+					<p style={{ color: 'black'}} className='pt-2'>London</p>
 
-						<LanguagesList />
-					</div>
+					<LanguagesList />
+				</div>
 			</div>
 
 			<div
@@ -37,9 +33,6 @@ export const ProfileCard = () => {
 				style={{ backgroundColor: '#C4C4C4', height: '155px' }}
 			>
 				<SocialsList />
-				{/* <div style={{backgroundColor: '#C4C4C4'}}> */}
-				{/* <ContactStrip /> */}
-				{/* </div> */}
 			</div>
 		</div>
 	)
@@ -48,23 +41,35 @@ const LanguagesList = () => {
 	const stripPathing = string => string.match(/(?:.\/)+(\w*)+(?:\.svg)/)[1]
 	return (
 		<div className='flex flex-row justify-around pt-6 h-16 justify-around'>
-			{svgs.map(svg => (
-				<img src={svg.file} className='h-full px-1' alt={`${stripPathing(svg.path)} logo`} />
+			{svgs.map((svg, idx) => (
+				<img
+					key={idx}
+					src={svg.file}
+					className='h-full px-1'
+					alt={`${stripPathing(svg.path)} logo`}
+				/>
 			))}
 		</div>
 	)
 }
+
 const SocialsList = () => {
 	return (
 		<div className='flex flex-row justify-around pt-6 h-32 justify-around w-4/5'>
-			{socials.map(social => (
-				<a href={social.url} target="_blank" >
-					<img src={social.icon} className='h-16 w-16 px-1' alt={`${social.platform} logo`} />
+			{socials.map((social, idx) => (
+				<a href={social.url} target='_blank'>
+					<img
+						key={idx}
+						src={social.icon}
+						className='h-16 w-16 px-1'
+						alt={`${social.platform} logo`}
+					/>
 				</a>
 			))}
-		</div>	
+		</div>
 	)
 }
+
 const socials = [
 	{
 		platform: 'twitter',
