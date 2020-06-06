6afedc798bdad5668f5c76002bce481350dc42d3
Kieran Osgood <kieran@analogrepublic.com>
2020-06-06 17:47:15 +0100

    addd usecallback to usewindowsize

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from 'react'
 // Hook
 export default function useWindowSize() {
-    const isClient = typeof window === 'object';
-  
-    function getSize() {
-      return {
-        width: isClient ? window.innerWidth : undefined,
-        height: isClient ? window.innerHeight : undefined
-      };
-    }
-  
-    const [windowSize, setWindowSize] = useState(getSize);
-  
-    useEffect(() => {
-      if (!isClient) {
-        return false;
-      }
-      
-      function handleResize() {
-        setWindowSize(getSize());
-      }
-  
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []); // Empty array ensures that effect is only run on mount and unmount
-  
-    return windowSize;
-  }
+	const isClient = typeof window === 'object'
+
+	const getSize = useCallback(() => {
+		return {
+			width: isClient ? window.innerWidth : undefined,
+			height: isClient ? window.innerHeight : undefined,
+		}
+	}, [isClient])
+
+	const [windowSize, setWindowSize] = useState(getSize)
+
+	useEffect(() => {
+		if (!isClient) {
+			return false
+		}
+
+		function handleResize() {
+			setWindowSize(getSize)
+		}
+
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [isClient, getSize]) 
+
+	return windowSize
+}
