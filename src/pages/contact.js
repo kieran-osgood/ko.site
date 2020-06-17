1158bee989e6558a08bd5e8981447253c9190d44
Kieran Osgood <kieran@analogrepublic.com>
2020-06-17 17:45:37 +0100

    feat(contact): add basic form and styling to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import tw, { styled } from 'twin.macro'
 
 import Layout from 'components/layout'
 import SEO from 'components/seo'
@@ -7,10 +8,48 @@ import HighlightedLine from 'components/highlightedline'
 const Contact = ({ path }) => (
 	<Layout path={path}>
 		<SEO title='Contact' />
+<div tw='grid gap-8'>
 
 		<HighlightedLine highlightText={`Contact`} />
 
+		<p tw='text-base'>
+			I currently have some availability for side work alongside my current job,
+			so use the form below if you want to discuss!
+		</p>
+
+		<Form />
+</div>
 	</Layout>
 )
 
 export default Contact
+
+const Form = () => (
+	<form netlify tw='grid sm:grid-cols-2 gap-10 row-gap-6'>
+		<Label htmlFor='firstName'>
+			First Name
+			<Input type='text' name='firstName' placeholder='John' />
+		</Label>
+		<Label htmlFor='lastName'>
+			Last Name
+			<Input type='text' name='lastName' placeholder='Smith' />
+		</Label>
+		<Label htmlFor='email'>
+			Email Address:
+			<Input type='text' name='email' placeholder='johnsmith@gmail.com' />
+		</Label>
+		<Label htmlFor='phoneNumber'>
+			Phone Number:
+			<Input type='text' name='phoneNumber' placeholder='XXX XXX XXXX' />
+		</Label>
+
+		<Input type='submit' value="Send" tw='py-6 text-base hover:cursor-pointer text-secondary-text' />
+	</form>
+)
+
+const Label = styled.label`
+	${tw`flex flex-col justify-between text-base`}
+`
+const Input = styled.input`
+	${tw`px-4 py-2 rounded-lg shadow-lg`}
+`
