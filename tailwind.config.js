c8fae4e348993424c2adad24a14cb6afcd15800f
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-20 16:45:07 +0000

    chore: disable darkmode in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
