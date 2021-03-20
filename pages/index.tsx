8f48f3bdcd6745bad620eabb6ddf86adf51b71b4
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-20 16:42:35 +0000

    feat(Index): add Hero callout section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from 'src/components/layout'
-import ImageSlider from 'src/components/imageslider'
-import HighlightedLine from 'src/components/highlightedline'
+import Layout from 'src/components/layout';
+import Heading from 'src/components/heading';
 
-const IndexPage = ({ path }) => {
+type IndexProps = {
+	path: string;
+};
+
+const IndexPage = ({ path }: IndexProps) => {
 	return (
-	<Layout path={path}>
-hello
-	</Layout>
-)}
+		<Layout path={path}>
+			<HeroMessage />
+		</Layout>
+	);
+};
 
-export default IndexPage
+export default IndexPage;
+
+const HeroMessage = () => {
+	return (
+		<>
+			<Heading h='h1'>Hello, my</Heading>
+			<Heading h='h1'>name&apos;s Kieran.</Heading>
+			<Heading h='h1'>I&apos;m a freelance</Heading>
+			<Heading h='h1'>Software Engineer.</Heading>
+		</>
+	);
+};
