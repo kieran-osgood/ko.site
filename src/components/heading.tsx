fffeb7be2f5e2e70e92a63a4263fa8d704c322fb
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-21 19:54:54 +0000

    feat: adjust h1 size due to tailwind config change

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -9,7 +9,7 @@ const Heading = ({ h = 'h1', children }: HeadingProps) => {
 	switch (h) {
 	case 'h1':
 		return (
-			<h1 className='text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold leading-tight'>{children}</h1>
+			<h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-semibold leading-tight'>{children}</h1>
 		);
 	case 'h2':
 		return <h2>{children}</h2>;
