9bb93a1d713b53859992f6fc2b674e4c7fa14376
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-21 18:51:53 +0000

    feat(Layout): update heaedr with new logo and navigation items

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,44 +1,24 @@
-import React, { createContext, useState, useEffect } from 'react';
+import * as React from 'react';
 import Link from 'next/link';
+import Footer from 'src/components/footer';
+import LogoSVG from 'assets/logo.svg';
 
-import Toggle from 'react-toggle';
-
-import { DEFAULT_THEME } from '../themes';
-import { applyTheme } from '../themes/utils';
-
-import Footer from './footer';
-import Navigation from './navigation';
-
-import Sun from '../../assets/sun.svg';
-import Moon from '../../assets/moon.svg';
-import LogoSVG from '../../assets/logo.svg';
-import useLocalStorage from '../hooks/useLocalStorage';
-import config from 'src/config';
-
-export const Context = createContext<string | null>(null);
+export const Context = React.createContext<string | null>(null);
 
 type LayoutProps = {
 	children: React.ReactNode;
 	path: string;
 };
-
 const Layout = ({ children, path }: LayoutProps) => {
-	const [theme, setTheme] = useLocalStorage('theme', DEFAULT_THEME); // refactor into uselocalstorage
-
-	useEffect(() => {
-		applyTheme(theme);
-	}, [theme]);
-
 	return (
 		<Context.Provider value={path}>
 			<div className='bg-primary-background min-h-screen text-primary-text'>
+				<Header />
 				<main className='w-full h-full relative block mx-auto'>
-					<Header />
 					<div className='mx-auto pb-6 pt-16 px-8 lg:max-w-screen-md xl:w-3/4 xl:max-w-screen-lg break-words'>
 						{children}
 					</div>
 				</main>
-				<Navigation path={path} siteTitle={config.title} />
 				<Footer />
 			</div>
 		</Context.Provider>
@@ -47,44 +27,58 @@ const Layout = ({ children, path }: LayoutProps) => {
 
 export default Layout;
 
-// const Main = styled.main(() => [
-// 	css`
-// 		max-width: 100vw;
-// 		min-height: calc(100vh - 184px);
-// 		@media (min-width: 1024px) {
-// 			min-height: calc(100vh - 119px);
-// 		}
-// 	`,
-// ]);
-
 const Header = () => {
 	return (
-		<>
-			<div className='flex justify-between items-center mx-auto h-36 pt-8 pl-8 pr-16 lg:max-w-screen-md xl:w-3/4 xl:max-w-screen-lg break-words'>
-				<Link href='/'>
-					<a className='h-full hover:opacity-75'>
-						<LogoSVG
-							className='w-auto h-full'
-							style={{
-								fill: 'none',
-								rect: {
-									stroke: 'var(--color-text-primary)',
-								},
-								path: {
-									fill: 'var(--color-text-primary)',
-								},
-							}}
-						/>
-					</a>
-				</Link>
-				{/* <Toggle
-							defaultChecked={!theme}
-							onClick={() => setTheme(theme === 'base' ? 'dark' : 'base')}
-							icons={{ unchecked: <Moon />, checked: <Sun /> }}
-							aria-label='Theme switcher'
-							className='dark-mode-toggle'
-						/> */}
-			</div>
-		</>
+		<div className='flex justify-between items-center mx-auto pt-8 pl-8 pr-16 lg:max-w-screen-md xl:w-3/4 xl:max-w-screen-lg break-words'>
+			<Link href='/'>
+				<a className='h-10 hover:opacity-75'>
+					<LogoSVG className='h-full w-auto'/>
+				</a>
+			</Link>
+			{/* <HeaderNavigation /> */}
+		</div>
 	);
 };
+
+const HeaderNavigation = () => {
+	return (
+		<div className='text-2xl font-semibold'>
+			{navigation.map((nav,idx) => (
+				<Link key={idx} href={nav.url}>
+					<a className='mr-12'>{nav.name}</a>
+				</Link>
+			))}
+		</div>
+	);
+};
+
+type NavItems = {
+	name:string;
+	url:string;
+}[];
+const navigation: NavItems = [
+	{
+		name: 'Home',
+		url: '/',
+	},
+	{
+		name: 'About',
+		url: '/about',
+	},
+	{
+		name: 'Portfolio',
+		url: '/portfolio',
+	},
+	{
+		name: 'Blog',
+		url: '/blog',
+	},
+	{
+		name: 'Uses',
+		url: '/uses',
+	},
+	{
+		name: 'Contact',
+		url: '/contact',
+	},
+];
