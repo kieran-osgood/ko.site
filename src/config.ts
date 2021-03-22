b81f638e4cc3eae311eb89f113faf2bf7728eaf7
Kieran Osgood <kieranbosgood@gmail.com>
2021-03-22 07:50:57 +0000

    chore(config): format config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export default {
-  title: 'KO.Dev',
-  author: {
-    name: 'Kieran Osgood',
-    summary: 'Software Engineer based in London, UK.',
-  },
-  description: 'This is a personal website for Kieran Osgood, built by Kieran Osgood, utilising Next.js, React, Typescript, GraphQL, TailwindCSS and PurgeCSS.',
-  social: {
-    twitter: '',
-  },
-}
+	title: 'KO.Dev',
+	author: {
+		name: 'Kieran Osgood',
+		summary: 'Software Engineer based in London, UK.',
+	},
+	description: 'This is a personal website for Kieran Osgood, built by Kieran Osgood, utilising Next.js, React, Typescript, GraphQL, TailwindCSS and PurgeCSS.',
+	social: {
+		twitter: '',
+	},
+};
