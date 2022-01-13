ea3a27829fea4b38936b7ac83f2f3fb349355b25
Kieran Osgood <kieranbosgood@gmail.com>
2022-01-13 14:55:20 +0000

    chore: commit files (machine migration)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-
+import thisCanBeAnything from 'src/styles/css/hero.module.scss';
+import another from 'src/styles/css/another.module.scss';
 import Layout from 'src/components/layout';
 import Heading from 'src/components/heading';
 
@@ -7,8 +8,13 @@ type IndexProps = {
 	path: string;
 };
 const IndexPage = ({ path }: IndexProps) => {
+	console.log(thisCanBeAnything);
 	return (
 		<Layout path={path}>
+			{/* <div className={`${thisCanBeAnything.aname} somethigelse`}>styles</div> */}
+			<div className={`${thisCanBeAnything.aname} ${another.aname}`}>another</div>
+			<div className="aname"></div>
+			<HeroBackground/>
 			<HeroMessage />
 		</Layout>
 	);
@@ -24,5 +30,13 @@ const HeroMessage = () => {
 			<Heading h='h1'>I&apos;m a freelance</Heading>
 			<Heading h='h1'>Software Engineer.</Heading>
 		</>
+	);
+};
+
+const HeroBackground = () => {
+	return (
+		<div className={thisCanBeAnything}>
+
+		</div>
 	);
 };
