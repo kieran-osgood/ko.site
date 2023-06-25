6d211b4ce4c7be5a729cd0d33d1930fbc21f5521
Kieran Osgood <kieran.osgood@and.digital>
2023-06-25 16:00:03 +0100

    feat: setup tailwind and being adding footer component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    tailwind(),
     starlight({
       title: 'KO',
       social: {
