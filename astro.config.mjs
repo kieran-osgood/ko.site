b4c705ff80e77ff9785a9b05c49decdc6274b316
Kieran Osgood <kieran.osgood@and.digital>
2023-06-25 15:45:08 +0100

    feat: init astro project to replace gatsby

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'KO',
+      social: {
+        github: 'https://github.com/kieran-osgood',
+        linkedin: 'https://www.linkedin.com/in/kieranosgood',
+        twitter: 'https://twitter.com/kieranbosgood',
+      },
+      // sidebar: [
+        // {
+        //   label: 'Guides',
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: 'Example Guide', link: '/guides/example/' },
+        //   ],
+        // },
+        // {
+        //   label: 'Reference',
+        //   autogenerate: { directory: 'reference' },
+        // },
+      // ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
