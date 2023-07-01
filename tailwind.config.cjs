b51eea4db3d1871c9827ad81b456c2330c624e17
Kieran Osgood <kieranbosgood@gmail.com>
2023-07-01 20:14:25 +0100

    feat: setup layouts for screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+import preline from 'preline/plugin.js';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './public/**/*.astro',
+        'node_modules/preline/dist/*.js',
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
-}
+    plugins: [preline],
+    darkMode: 'class',
+    // darkMode: 'media',
+
+
+};
