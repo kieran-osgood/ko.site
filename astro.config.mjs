78827d79726ccc313e02a3c8a7e76f63c69892d8
Kieran Osgood <kieranbosgood@gmail.com>
2024-04-20 17:20:45 +0100

    feat: add react support and astro check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
+      entrypoint: "astro/assets/services/sharp"
+    }
   },
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
+    remarkPlugins: [remarkReadingTime]
+  }
 });
