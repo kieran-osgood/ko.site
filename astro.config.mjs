09c239205303706584e9a60d674f6422fb03e20e
Kieran Osgood <kieranbosgood@gmail.com>
2024-04-21 08:24:13 +0100

    fix: astro ts errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,20 @@ import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
+      entrypoint: "astro/assets/services/sharp"
+    }
   },
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
+  output: "server",
+  adapter: vercel()
 });
