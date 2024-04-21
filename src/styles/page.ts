ddb716cbd52797e59aefa44c6c4bfdfcd7846622
Kieran Osgood <kieranbosgood@gmail.com>
2024-04-21 07:52:52 +0100

    refactor(navigation): re-use highlight function in header/footer

--- src/styles/page.ts
+++ src/styles/page.ts
@@ -1,2 +1,16 @@
 export const pageBounds =
   "w-full sm:max-w-xl md:max-w-3xl lg:max-w-7xl self-center";
+
+export const highlightCurrentPage = (selectors: string) => {
+  const anchors = document.querySelectorAll<HTMLAnchorElement>(selectors);
+  for (const _ of anchors) {
+    if (_.pathname === "/" && window.location.pathname !== "/") {
+      continue;
+    }
+
+    if (window.location.pathname.startsWith(_.pathname)) {
+      _.classList.add("text-blue-400", "dark:!text-blue-400"); // important used to override default text color
+      break; // we only want to attach highlight to first node
+    }
+  }
+};
