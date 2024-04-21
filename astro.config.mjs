d5034de3d727d286b620191567b46b34e1d8364e
Kieran Osgood <kieranbosgood@gmail.com>
2024-04-21 07:53:19 +0100

    feat: set tsc config to astro strictest

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -11,10 +10,10 @@ export default defineConfig({
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: "astro/assets/services/sharp"
-    }
+      entrypoint: "astro/assets/services/sharp",
+    },
   },
   markdown: {
-    remarkPlugins: [remarkReadingTime]
-  }
+    remarkPlugins: [remarkReadingTime],
+  },
 });
