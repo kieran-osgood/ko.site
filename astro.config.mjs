9d979201765151a99a8ae22eee98ff8988e56b67
Kieran Osgood <kieranbosgood@gmail.com>
2024-04-24 17:08:17 +0100

    refactor: move around files to group by discipline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import pagefind from "astro-pagefind";
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,10 @@
 import { defineCollection, z } from "astro:content";
 
-const date = z.union([z.number(), z.string(), z.date()]).pipe(z.coerce.date());
-
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    date: date,
+    date: z.union([z.number(), z.string(), z.date()]).pipe(z.coerce.date()),
   }),
 });
 
