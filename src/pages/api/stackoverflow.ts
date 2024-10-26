0dd8bb58ee20085cabfdd84912291f36e6998123
Kieran Osgood <kieranbosgood@gmail.com>
2024-10-26 11:44:01 +0100

    feat(Stackoverflow): wrap in card - migrate to astro components

--- src/pages/api/stackoverflow.ts
+++ src/pages/api/stackoverflow.ts
@@ -4,6 +4,28 @@ import { z } from "astro/zod";
 const ROUTE =
   "https://api.stackexchange.com/2.2/users/5428936?site=stackoverflow";
 
+export type StackOverflowStats = {
+  badge_counts: Record<"bronze" | "silver" | "gold", number>;
+  // account_id: number;
+  // is_employee: boolean;
+  // last_modified_date: number;
+  // last_access_date: number;
+  // reputation_change_year: number;
+  // reputation_change_quarter: number;
+  // reputation_change_month: number;
+  // reputation_change_week: number;
+  // reputation_change_day: number;
+  reputation: number;
+  // creation_date: number;
+  // user_type: "registered";
+  // user_id: number;
+  // location: string;
+  // website_url: string;
+  link: string;
+  // profile_image: string;
+  // display_name: string;
+};
+
 // NOTE: schema are intentionally sparse and missing properties
 export const StackOverflow = z.object({
   items: z.array(z.any()),
